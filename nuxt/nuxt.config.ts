36f80dc98ed02a333552282f41498d260a328640
duvigneau <thomas@duvigneau.io>
2023-10-31 14:22:33 +0100

    Add clean nuxt project

---
+++ nuxt/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/sanity'],
+  sanity: {
+    projectId: 'xq5d260q'
+  },
+  devtools: { enabled: true }
+})
